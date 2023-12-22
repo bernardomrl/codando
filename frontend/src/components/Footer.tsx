c2a3bc3520e9ae6a3a8ce4aad8411d8f96e3c320
Bernardo Meirelles <bernardomrl@icloud.com>
2023-12-22 14:27:34 -0300

    updated footer component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,21 @@
+import Link from 'next/link';
+
 export default function Footer() {
   return (
-    <footer className="flex flex-col justify-center items-center p-4 w-full"></footer>
+    <footer className="flex fixed bottom-0 flex-col justify-center items-center p-4 w-full">
+      <h3 className="text-sm font-nunito font-semibold opacity-25 text-center">
+        projeto desenvolvido por{' '}
+        <Link
+          href="https://github.com/bernardomrl"
+          className="link link-hover "
+        >
+          @bernardomrl
+        </Link>{' '}
+        e{' '}
+        <Link href="https://github.com/nunes262" className="link link-hover ">
+          @nunes262
+        </Link>
+      </h3>
+    </footer>
   );
 }
